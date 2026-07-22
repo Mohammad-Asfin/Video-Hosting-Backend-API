59c54e85c513bb9175b3bfd551ce76449aed65cf
Mohammad Asfin <mdasfin0gmail.com>
2026-07-22 16:41:20 +0530

    feat: implement comment and tweet routes while removing bad-words dependency

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -8,14 +8,13 @@ import {
 } from "../controllers/comment.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 import {commentLimiter} from "../middlewares/rateLimiter.js"
-import {moderateContent} from "../middlewares/moderation.js"
 
 const router = Router();
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/:videoId").get(getVideoComments).post(commentLimiter, moderateContent, addComment);
-router.route("/c/:commentId").delete(deleteComment).patch(moderateContent, updateComment);
+router.route("/:videoId").get(getVideoComments).post(commentLimiter, addComment);
+router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
 router.route("/c/:commentId/replies").get(getCommentReplies);
 
 export default router
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -8,14 +8,13 @@ import {
 } from "../controllers/tweet.controller.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
-import {moderateContent} from "../middlewares/moderation.js"
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(upload.single("image"), moderateContent, createTweet);
+router.route("/").post(upload.single("image"), createTweet);
 router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(moderateContent, updateTweet).delete(deleteTweet);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 router.route("/:tweetId/vote").patch(voteOnPoll);
 
 export default router
